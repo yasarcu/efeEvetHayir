d060f746b91915304fdc53a4b57ac1cd6ef15a1e
unknown <ztb62@ANKARAHASTANESI.LOCAL>
2022-10-26 12:22:16 +0300

    ilk hece ve ses testi

--- mete.js
+++ mete.js
@@ -5,6 +5,7 @@
 //     document.getElementById('metefe').style.width = genislik + 'px';
 //     document.getElementById('metefe').style.height = uzunluk + 'px';
 // }
+
 function fareUzerinde(bir, iki) {
     play(bir, iki)
   }
@@ -16,27 +17,24 @@ function play(deger, _deger) {
     }
     var audio = document.getElementById(deger);
     // ikinci defer buttona tiklaninca tekrar eski rengine donsun hatasina bak
-    var sayiBack = _deger.style.backgroundColor;
-    if (sayiBack != "rgb(227, 26, 23)") {
-        _deger.style.backgroundColor = "#e31a17";
-    }
-    else {
-        _deger.style.backgroundColor = "#000";
-    }
-    // besceTiklaAc = 3;
-    // document.getElementById('kos').textContent = "Kosoferta ";
-    // @ts-ignore
+    // var sayiBack = _deger.style.backgroundColor;
+    // if (sayiBack != "rgb(227, 26, 23)") {
+    //     _deger.style.backgroundColor = "#e31a17";
+    // }
+    // else {
+    //     _deger.style.backgroundColor = "#000";
+    // }
     audio.play();
 }
-function isimler() {
-    let isim = document.getElementById("metefe").textContent;
-    if(isim == "Efe") {
-        document.getElementById('metefe').textContent = "Mete";
+// function isimler() {
+//     let isim = document.getElementById("metefe").textContent;
+//     if(isim == "Efe") {
+//         document.getElementById('metefe').textContent = "Mete";
 
-    } else {
-    document.getElementById('metefe').textContent = "Efe";
-}
-}
+//     } else {
+//     document.getElementById('metefe').textContent = "Efe";
+// }
+// }
  
 // if(document.getElementById("metefe").textContent = "Mete")
 
